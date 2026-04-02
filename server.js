ee21d8eb10a5492fc4b6385e04496e9f1a32451f
Hugo Gordon <gordonhugo23@gmail.com>
2026-04-02 16:42:07 +1000

    Add sitemap.xml route and canonical tag

--- server.js
+++ server.js
@@ -107,6 +107,21 @@ const server = http.createServer(async (req, res) => {
     serveFile(res, path.join(__dirname, req.url.split('?')[0]), MIME_TYPES[ext]); return;
   }
 
+  // --- Sitemap ---
+  if (req.method === 'GET' && req.url === '/sitemap.xml') {
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://revaixstudy.com/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+    res.writeHead(200, { 'Content-Type': 'application/xml' });
+    res.end(sitemap);
+    return;
+  }
+
   // --- Anthropic API proxy ---
   if (req.method === 'POST' && req.url === '/api/messages') {
     const body = await collectBody(req);
